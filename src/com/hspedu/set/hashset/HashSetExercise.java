262524a13a8a6e2c32de680a99739d7ebcda1437
Qiny-M <mengiqngyi999@gmail.com>
2023-11-27 16:35:12 +0800

    HashSetExercise二次修改

--- src/com/hspedu/set/hashset/HashSetExercise.java
+++ src/com/hspedu/set/hashset/HashSetExercise.java
@@ -6,14 +6,45 @@ import java.util.Set;
 @SuppressWarnings({"all"})
 public class HashSetExercise {
     public static void main(String[] args) {
+
+        /*
+        * hashCode值与equals是否有关系呢？答案是肯定的，
+        * 如果使用equals()方法比较两个对象得到true，那么这两个对象的hashCode必须是相同的。
+        * 需要注意的是：这里所指的true是，使用Object类中的equals()方法比较两个对象得到的true。
+        *
+        * 这也就要求了当继承了Object类的一个类需要重写equals()方法来判断相等逻辑时，
+        * 也要同时重写hashCode()方法来返回与equals()判断逻辑一致的hashCode值。
+        * String类重写了equals方法，所以当equals判断相等时，必须返回给两个对象相同的hashCode值
+        *
+        *
+        * 因此总结equals()与hashCode的关系是：
+
+        * 1）hashCode相等的两个对象，equals()返回的不一定是true。
+        *********************************************
+        * 2）equals()返回为true时，hashCode一定相等。***
+        * ******************************************/
+
         Set set = new HashSet();
-//        set.add(new Dog("tom"));
-//        set.add(new Dog("jack"));
+        Dog tom1 = new Dog("tom");
+        Dog tom2 = new Dog("tom");
+        System.out.println(tom1.hashCode());
+        System.out.println(tom2.hashCode());
+        set.add(tom1);
+        set.add(tom2);
 //        System.out.println("set=" + set);
 
-        set.add(new String("jim"));
-        set.add(new String("jim"));
+        String a="jim";
+        String jim1 = new String("jiiim");
+        String jim2 = new String("jiiim");
+        String jim3 = new String("jim");
+        set.add(jim1);
+        set.add(jim2);
+        System.out.println(a.hashCode());
+        System.out.println(jim1.hashCode());
+        System.out.println(jim2.hashCode());
+        System.out.println(jim3.hashCode());
         System.out.println("set=" + set);
+
     }
 }
 
